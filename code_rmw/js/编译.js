c22f853ac3064f97060ab26e34f8b577020b1c90
gcxfd <i@rmw.link>
2022-04-11 13:02:50 +0800

    🔶

--- code_rmw/js/编译.js
+++ code_rmw/js/编译.js
@@ -1,11 +1,14 @@
-import * as 态 from ./态
+import * as 态 from './态'
 
 import 词法 from './词法'
 
 export default (源码,模块依赖)=>{
   const 结果=词法(源码)
-  结果.map((行,列,state,...li)=>{
-    if(态==)
+  结果.map(([行,列,state,...li])=>{
+    console.log(state)
+    if(态.导入==state){
+      console.log(li)
+    }
   })
   return ''
 }
