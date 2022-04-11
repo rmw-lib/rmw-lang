a200e5e6080fad6fecbac0fe917ea6e8364e3b29
gcxfd <i@rmw.link>
2022-04-11 18:10:09 +0800

    🔶

--- code_rmw/js/词法.js
+++ code_rmw/js/词法.js
@@ -3,26 +3,22 @@ import * as 态 from './态'
 import { 行缩进 } from './词法/小函数'
 import 导入 from './词法/导入'
 
-export default (源码)=>{
-  源码=源码.replaceAll('\r\n','\n').replaceAll('\r','\n').split('\n')
+export default (源)=>{
+  源=源.replaceAll('\r\n','\n').replaceAll('\r','\n').split('\n')
   let 行=0,
     列=0,
     行列
-  const 源码行数=源码.length,
+  const 源行数=源.length,
     果=[],
-    返=果.push.bind(果)
-  while(源码行数>行){
-    行列=导入(返,源码,行,列)
-    if(Array.isArray(行列)){
-      [行,列]=行列
-    }else{
-      if(Number.isInteger(行列)){
-        if(行列>行){
-          列=0
-          行=行列
-          continue
-        }
-      }
+    返=(...参)=>{
+      果.push(参)
+    }
+  while(源行数>行){
+    列=0
+    行=导入(返,源,行,列)
+    if(行列>行){
+      行=行列
+      continue
     }
     ++行
   }
